eb4bb49fda9171bbfa5c675e6146274356bf9f1f
Mouldi GABSI <gabsi.mouldi@gmail.com>
2021-02-01 19:11:04 +0100

    Add_encoding_and_Endpoint

---
+++ Network/APIQueryParam.swift
@@ -0,0 +1,8 @@
+//
+//  APIQueryParam.swift
+//  LBCAnnouncementsCore
+//
+//  Created by Mouldi GABSI on 01/02/2021.
+//
+
+import Foundation
---
+++ Network/Encoding/URLParameterEncoder.swift
@@ -0,0 +1,8 @@
+//
+//  URLParameterEncoder.swift
+//  LBCAnnouncementsCore
+//
+//  Created by Mouldi GABSI on 01/02/2021.
+//
+
+import Foundation
